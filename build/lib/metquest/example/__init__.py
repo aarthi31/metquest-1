3b0808a32869c27e1782a2d870b753c203ca84db
Aarthi <bt13d031@smail.iitm.ac.in>
2018-05-04 16:35:33 +0530

    deleted build, dist and egg-info

--- build/lib/metquest/example/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-from os.path import abspath, dirname, join
-from .run_this_example import *
--- build/scripts-3.6/run_metquest.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/python3
-
-import metquest
-metquest.execute_all_codes()
