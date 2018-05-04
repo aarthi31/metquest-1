d46030a6a84e86bb8a25429207a4c02c590fb27c
Aarthi Ravikrishnan <bt13d031@smail.iitm.ac.in>
2018-05-04 17:49:30 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -179,13 +179,15 @@ texinfo_documents = [
 intersphinx_mapping = {'https://docs.python.org/': None}
 
 # -- sphinx-apidoc calling ---------------------------------------------
-
 def run_apidoc(_):
-    from sphinx.apidoc import main
-    mod_path = join(PROJECT_ROOT, 'metquest')
-    main([None, '-f', '-d', '2', '-e', '-o', mod_path])
-
+	from sphinx.apidoc import main
+	import os
+	import sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+	cur_dir = os.path.abspath(os.path.dirname(__file__))
+	module = os.path.join(cur_dir,"..","metquest")
+	main(['-e', '-o', cur_dir, module, '--force'])
 
 def setup(app):
-    app.connect('builder-inited', run_apidoc)
-
+	app.connect('builder-inited', run_apidoc)
+    
