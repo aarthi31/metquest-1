0548430705ecd69d4bb9d742ec78b07dc6adbc90
Aarthi Ravikrishnan <bt13d031@smail.iitm.ac.in>
2018-05-04 17:05:59 +0530

    Update conf.py
    Added  'sphinx.ext.napoleon' to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
