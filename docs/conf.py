21316a79264ce3e2a1d65f11c8ed68cc6fc407cc
Aarthi Ravikrishnan <bt13d031@smail.iitm.ac.in>
2018-05-04 19:09:20 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ copyright = '2018, Aarthi Ravikrishnan'
 author = 'Aarthi Ravikrishnan'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.26'
+release = '0.1.28'
 
 
 # -- General configuration ---------------------------------------------------
